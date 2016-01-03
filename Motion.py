75590106eedc18a6f5d32e576f6db6ba99945362
GesusK <zhuyixiao@gmail.com>
2016-01-03 13:15:16 -0500

    improve the forwardbyAngle and backwardbyAngle methods:  1. reset position and then a main run resulting in coarse result position;  2. using speed 100, then 50 to repeat the running, without reset

--- Motion.py
+++ Motion.py
@@ -26,8 +26,9 @@ class Driver():
   run_sp=500 # running speed
   t_sp=400 # turning speed
 
-  t_accuracy=2 # degree
-  r_accuracy=5 # mm
+  t_accuracy=2 # turning accuracy for Gyro, in degree
+  r_accuracy=5 # running accuracy for Motor position, in degree
+  adjust_sp = [100, 50]
 
   def __init__(self):
     self.left=Motor(port=Motor.PORT.B)
@@ -53,11 +54,14 @@ class Driver():
     angle = int(abs(dist*10)/self.Wper*360)
     self.forwardbyAngle(angle)
 
+    #self.forwardbySecond()
+
   def forwardbyAngle(self, angle):
     angtruth = [0, 0]
     angtmp = [angle, angle]
 
     # moving and adjusting loop
+    loop_counter = 0;
     while(abs(angtmp[0])>=self.r_accuracy and abs(angtmp[1]) >=self.r_accuracy):
       angtruth = self.oneAngleForward(angtmp[0], angtmp[1])
       angtmp[0]=angtmp[0]-angtruth[0]
@@ -65,24 +69,43 @@ class Driver():
       print "fore New angtmp: "+ str(angtmp)
 
   def oneAngleForward(self, left_ang, right_ang):
-    self.reset()
-    self.left.position=0
-    self.left.setup_position_limited(position_sp=left_ang, speed_sp=self.run_sp,
-                                     stop_mode=Motor.STOP_MODE.BRAKE)
-    self.right.position=0
-    self.right.setup_position_limited(position_sp=right_ang, speed_sp=self.run_sp,
-                                      stop_mode=Motor.STOP_MODE.BRAKE)
 
+    # run the main movement
+    self.reset()
+    self.left.setup_position_limited(position_sp=left_ang, speed_sp=self.run_sp,
+                                     stop_mode=Motor.STOP_MODE.HOLD, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
+    self.right.setup_position_limited(position_sp=right_ang, speed_sp=self.run_sp,
+                                      stop_mode=Motor.STOP_MODE.HOLD, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
     self.left.start()
     self.right.start()
-    #time.sleep(10)
+    time.sleep(10)  # need to figure out how much needed
     self.stop()
+
+    # adjust using low speed mode
+    for v_adj in self.adjust_sp:
+        self.left.setup_position_limited(position_sp=left_ang, speed_sp=v_adj,
+                                         stop_mode=Motor.STOP_MODE.HOLD, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
+        self.right.setup_position_limited(position_sp=right_ang, speed_sp=v_adj,
+                                          stop_mode=Motor.STOP_MODE.HOLD, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
+        self.left.start()
+        self.right.start()
+        time.sleep(1)
+
     return [self.left.position, self.right.position]
+
+  def forwardbySecond(self, sec):
+    self.reset();
+    self.left.setup_time_limited(time_sp=sec, speed_sp=self.run_sp, speed_regulation=True)
+    self.right.setup_time_limited(time_sp=sec, speed_sp=self.run_sp, speed_regulation=True)
+    self.left.start()
+    self.right.start()
+    time.sleep(sec)
+    self.stop()
 
   def runBackward(self):
     self.reset()
-    self.left.setup_forever(-self.run_sp,speed_regulation = True)
-    self.right.setup_forever(-self.run_sp,speed_regulation = True)
+    self.left.setup_forever(-self.run_sp,speed_regulation=True)
+    self.right.setup_forever(-self.run_sp,speed_regulation=True)
     self.left.start()
     self.right.start()
 
@@ -99,23 +122,31 @@ class Driver():
     # moving and adjusting loop
     while(abs(angtmp[0])>=self.r_accuracy and abs(angtmp[1]) >=self.r_accuracy):
       angtruth = self.oneAngleForward(angtmp[0], angtmp[1])
-      angtmp[0]=angtmp[0]+angtruth[0]
-      angtmp[1]=angtmp[1]+angtruth[1]
+      angtmp[0] = angtmp[0]+angtruth[0]
+      angtmp[1] = angtmp[1]+angtruth[1]
       print "back New angtmp: "+ str(angtmp)
 
   def oneAngleBackward(self, left_ang, right_ang):
     self.reset()
-    self.left.position=0
-    self.left.setup_position_limited(position_sp=left_ang, speed_sp=self.run_sp,
-                                     stop_mode=Motor.STOP_MODE.BRAKE)
-    self.right.position=0
-    self.right.setup_position_limited(position_sp=right_ang, speed_sp=self.run_sp,
-                                      stop_mode=Motor.STOP_MODE.BRAKE)
+    self.left.setup_position_limited(position_sp=-left_ang, speed_sp=self.run_sp,
+                                     stop_mode=Motor.STOP_MODE.BRAKE, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
+    self.right.setup_position_limited(position_sp=-right_ang, speed_sp=self.run_sp,
+                                      stop_mode=Motor.STOP_MODE.BRAKE, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
 
     self.left.start()
     self.right.start()
-    #time.sleep(10)
+    time.sleep(10)
     self.stop()
+
+    # adjust using low speed mode
+    for v_adj in self.adjust_sp:
+        self.left.setup_position_limited(position_sp=-left_ang, speed_sp=v_adj,
+                                         stop_mode=Motor.STOP_MODE.HOLD, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
+        self.right.setup_position_limited(position_sp=-right_ang, speed_sp=v_adj,
+                                          stop_mode=Motor.STOP_MODE.HOLD, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
+        self.left.start()
+        self.right.start()
+        time.sleep(1)
     return [self.left.position, self.right.position]
 
   def turnRightbyAngle(self, ang):
@@ -131,10 +162,8 @@ class Driver():
     before=self.gyro.readAngle()
     #print "Before right turn: "+str(before)
     self.reset()
-    self.left.position=0
     self.left.setup_position_limited(position_sp=int(ang*self.Rrio), speed_sp=self.t_sp,
                                      stop_mode=Motor.STOP_MODE.BRAKE, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
-    self.right.position=0
     self.right.setup_position_limited(position_sp=-int(ang*self.Rrio), speed_sp=self.t_sp,
                                       stop_mode=Motor.STOP_MODE.BRAKE, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
     self.left.start()
@@ -159,10 +188,8 @@ class Driver():
     before=self.gyro.readAngle()
     #print "Before right turn: "+str(before)
     self.reset()
-    self.right.position=0
     self.right.setup_position_limited(position_sp=int(ang*self.Rrio), speed_sp=self.t_sp,
                                    stop_mode=Motor.STOP_MODE.BRAKE, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
-    self.left.position=0
     self.left.setup_position_limited(position_sp=-int(ang*self.Rrio), speed_sp=self.t_sp,
                                    stop_mode=Motor.STOP_MODE.BRAKE, ramp_up_sp=self.rmp_sp, ramp_down_sp=self.rmp_sp)
     self.right.start()
